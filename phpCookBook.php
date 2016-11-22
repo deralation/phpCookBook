6bdde7e2f4280adb848c109c6f7aca63d0f39d9a
gokhanDerala <gderala@gmail.com>
2016-11-22 23:35:44 +0300

    changes

--- phpCookBook.php
+++ phpCookBook.php
@@ -6,6 +6,9 @@ $name = "deralation";
 
 print substr($name,0,8)."<br>";
 
-print substr("watch out for that tree",17);
+print substr("watch out for that tree",17)."<br>";
+print substr("watch out for that tree",20,5)."<br>";
+print substr("watch out for that tree",-6)."<br>";
+print substr("watch out for that tree",-17,5)."<br>";
 
 ?>
