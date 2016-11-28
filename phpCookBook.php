014c8736b14de66c8fcf6fac412b47ec9f552c88
Gokhan Derala <gderala@gmail.com>
2016-11-28 17:46:38 +0200

    changes

--- phpCookBook.php
+++ phpCookBook.php
@@ -10,5 +10,8 @@ print substr("watch out for that tree",17)."<br>";
 print substr("watch out for that tree",20,5)."<br>";
 print substr("watch out for that tree",-6)."<br>";
 print substr("watch out for that tree",-17,5)."<br>";
+print substr("watch out for that tree",15,-2)."<br>";
+print substr("watch out for that tree",-4,-2)."<br>";
+
 
 ?>
